00f8355b089e5fc2a36cee34ca4301413e258279
bagher <sharifi80mb@gmail.com>
2021-08-30 21:50:41 +0430

    added food plan model again

--- Kafgir/Kafgir_API/models/food_plan.py
+++ Kafgir/Kafgir_API/models/food_plan.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+from django.db.models.deletion import SET_NULL
+
+from .food import Food
+from .user import User
+
 class FoodPlan(models.Model):
-    pass
+    date_time = models.DateTimeField()
+    breakfast = models.ForeignKey(Food, on_delete=SET_NULL, null=True)
+    lunch = models.ForeignKey(Food, on_delete=SET_NULL, null=True)
+    dinner = models.ForeignKey(Food, on_delete=SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=SET_NULL, null=True, related_name='food_plan')
