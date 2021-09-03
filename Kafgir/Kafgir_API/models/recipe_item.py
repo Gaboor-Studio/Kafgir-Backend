a8df1277e5a287dc3fd1902bc063db96564d1553
Amirparsa Sal <amirparsa.s@aut.ac.ir>
2021-09-04 02:50:15 +0430

    feat: create food related models

---
+++ Kafgir/Kafgir_API/models/recipe_item.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from .ingredient import Ingredient
+from .food import Food
+
+
+class RecipeItem(models.Model):
+
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='recipe_items')
+    text = models.TextField(null=False)
+    step = models.IntegerField()
--- Kafgir/Kafgir_API/models/shopping_list_item.py
+++ Kafgir/Kafgir_API/models/shopping_list_item.py
@@ -3,22 +3,12 @@ from django.db import models
 from django.db.models.deletion import SET_NULL
 
 from .user import User
+from .food import FoodUnit
 
 class ShoppingListItem(models.Model):
-    kg = 'kg'
-    g = 'g'
-    Number = 'Number'
-    Liter = 'Liter'
-
-    CHOICES = (
-        (kg,'kg'),
-        (g,"g"),
-        (Number,"Number"),
-        (Liter,"Liter")
-    )
 
     title = models.CharField(default='' ,max_length=255)
     done = models.BooleanField(default=False)
     amount = models.IntegerField(default=0)
-    unit = models.CharField(max_length=255, choices=CHOICES, default=kg)
+    unit = models.CharField(max_length=255, choices=FoodUnit.choices, default=FoodUnit.KILOGRAM)
     user = models.ForeignKey(User, on_delete=SET_NULL, null=True, related_name='shopping_list')
