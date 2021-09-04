6849043abff80ae76bae11b73ac4f326f796c2ba
Amirparsa Sal <amirparsa.s@aut.ac.ir>
2021-09-04 15:34:14 +0430

    fix: switch to mysql

--- Kafgir/Kafgir/settings.py
+++ Kafgir/Kafgir/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'Kafgir.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
     }
 }
 
