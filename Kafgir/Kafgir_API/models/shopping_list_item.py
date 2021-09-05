de5201953698fc11f22bd9081ad29412416d206c
bagher <sharifi80mb@gmail.com>
2021-09-05 15:26:26 +0430

    tag model changed

--- Kafgir/Kafgir_API/models/shopping_list_item.py
+++ Kafgir/Kafgir_API/models/shopping_list_item.py
@@ -3,12 +3,11 @@ from django.db import models
 from django.db.models.deletion import SET_NULL
 
 from .user import User
-from .food import FoodUnit
 
 class ShoppingListItem(models.Model):
 
     title = models.CharField(default='' ,max_length=255)
     done = models.BooleanField(default=False)
     amount = models.IntegerField(default=0)
-    unit = models.CharField(max_length=255, choices=FoodUnit.choices, default=FoodUnit.KILOGRAM)
+    unit = models.CharField(max_length=255, default='')
     user = models.ForeignKey(User, on_delete=SET_NULL, null=True, related_name='shopping_list')
