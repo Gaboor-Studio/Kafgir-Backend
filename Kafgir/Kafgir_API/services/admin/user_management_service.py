80299b7b8ca712d4a0a17014f1df0a4894993139
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2021-09-24 14:20:03 +0330

    fix: some changes in admin user management

--- Kafgir/Kafgir_API/services/admin/user_management_service.py
+++ Kafgir/Kafgir_API/services/admin/user_management_service.py
@@ -2,7 +2,7 @@ from dependency_injector.wiring import Provide, inject
 from typing import List
 from django.contrib.auth import get_user_model
 
-from ...exceptions.bad_request import UserAlreadyExistsException, UserIsAdmin
+from ...exceptions.bad_request import UserAlreadyExistsException
 from ...exceptions.not_found import UserNotFoundException
 from ...usecases.admin.user_management_usecases import UserManagementUsecase
 from ...mappers.user_management_mapper import UserManagementProfileMapper
@@ -26,13 +26,11 @@ class UserManagementService(UserManagementUsecase):
     
     def get_users_list(self) -> List[UserManagementProfileOutput]:
         ''' This method returns a list of users that are not admins'''
-        
         return list(map(self.user_management_profile_mapper.from_model, self.user_repo.get_all_by_is_staff(False)))
 
     
     def create_user(self, input: UserManagementCreateProfileInput) -> UserManagementProfileOutput:
         ''' Using this method, you can create a new user to the system and activate it at the same time'''
-        
         if self.user_repo.exist_user_by_username(input.username):
             raise UserAlreadyExistsException(detail=f'User(username={input.username}) already exists!')           
 
@@ -51,29 +49,19 @@ class UserManagementService(UserManagementUsecase):
     
     def edit_user(self, id: int, input: UserManagementEditProfileInput) -> UserManagementProfileOutput:
         ''' This method is used to edit a user's profile'''
-        
         try: 
-            user = self.user_repo.get_user_by_id(id)
+            user = self.user_repo.get_user_by_id_and_is_staff(id, False)
         except self.user_model.DoesNotExist:
             raise UserNotFoundException(f'User(id={id}) does not exist!')
 
-        if user.is_staff or user.is_superuser:
-            raise UserIsAdmin()
-
-        if input.username:
+        if input.username != user.username:
             if self.user_repo.exist_user_by_username(input.username):
                 raise UserAlreadyExistsException(detail=f'User(username={input.username}) already exists!') 
-            else:
-                user.username= input.username
-
-        if input.name:
-            user.name= input.name
-
-        if input.last_name:
-            user.last_name= input.last_name
-
-        if input.is_active:
-            user.is_active= input.is_active.lower() == 'true'
+            user.username= input.username
+        
+        user.name= input.name
+        user.last_name= input.last_name
+        user.is_active= input.is_active.lower() == 'true'
 
         self.user_repo.save_user(user)  
 
@@ -82,45 +70,27 @@ class UserManagementService(UserManagementUsecase):
     
     def delete_user(self, id:int) -> None:
         ''' Deletes the user with pk of given id'''
-        
         try: 
-            user = self.user_repo.get_user_by_id(id)
+            user = self.user_repo.get_user_by_id_and_is_staff(id, False)
         except self.user_model.DoesNotExist:
             raise UserNotFoundException(f'User(id={id}) does not exist!')
-
-        if user.is_staff or user.is_superuser:
-            raise UserIsAdmin()
 
         user.delete()
 
-    
     def set_user_password(self, id: int, input: UserManagementSetPasswordInput) -> None:
-        ''' This method sets the password for a user'''
-        
+        ''' This method sets the password for a user'''        
         try: 
-            user = self.user_repo.get_user_by_id(id)
+            user = self.user_repo.get_user_by_id_and_is_staff(id, False)
+            user.set_password(input.new_password)
+            self.user_repo.save_user(user)
         except self.user_model.DoesNotExist:
             raise UserNotFoundException(f'User(id={id}) does not exist!')
-
-        if user.is_staff or user.is_superuser:
-            raise UserIsAdmin()
-
-        user.set_password(input.new_password)
-        self.user_repo.save_user(user)
-
-    
+ 
     def set_user_pfp(self, id: int, input: UserManagementSetPfpInput) -> None:
         ''' This method sets pfp for a user'''
-        
         try: 
-            user = self.user_repo.get_user_by_id(id)
+            user = self.user_repo.get_user_by_id_and_is_staff(id, False)
+            user.image = input.image
+            self.user_repo.save_user(user)
         except self.user_model.DoesNotExist:
             raise UserNotFoundException(f'User(id={id}) does not exist!')
-
-        if user.is_staff or user.is_superuser:
-            raise UserIsAdmin()
-
-        user.image = input.image
-
-        self.user_repo.save_user(user)
-        
